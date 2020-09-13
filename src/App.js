58415410f736fb489e1f82a542283b96ec57a8f1
Andre-Junior <andrejunior.dias@gmail.com>
2020-09-13 20:13:20 -0300

    Tudo errado

--- src/App.js
+++ src/App.js
@@ -28,10 +28,30 @@ class App extends Component {
     super()
     this.state = {
       input: '',
-      imageUrl: ''
+      imageUrl: '',
+      box: {},
     }
   }
 
+  calculateFaceLocation = (data) => {
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputimage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.rightCol * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height)
+    }
+  }
+
+
+  displayFacebox = (box) => {
+    console.log(box)
+    this.setState({box: box})
+  }
+  
   onInputChange = (event) => {
     this.setState({input: event.target.value})
   }
@@ -42,14 +62,8 @@ class App extends Component {
       .predict(
         Clarifai.FACE_DETECT_MODEL,
         this.state.input)
-      .then(
-        function (response) {
-
-        },
-        function (err) {
-
-        }
-      )
+      .then(response => this.displayFacebox(this.calculateFaceLocation(response)))
+      .catch(err => console.log(err))
   }
 
   render() {
--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -4,7 +4,7 @@ const Facerecognition = ({ imageUrl }) => {
     return (
         <div className='center ma'>
             <div className='absolute mt2'>
-                <img src={imageUrl} alt='' width='500px' heigh='auto' />
+                <img id = 'inputimage' src={imageUrl} alt='' width='500px' heigh='auto' />
             </div>
         </div>
     )
