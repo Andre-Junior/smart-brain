eac0a21c3c4013e7e2b8377c140831385200182f
Andre-Junior <andrejunior.dias@gmail.com>
2020-09-19 17:29:31 -0300

    Register pt-BR
    O registro agora em português

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -6,9 +6,9 @@ const Register = ({ onRouteChange }) => {
             <main className="pa4 black-80">
                 <div className="measure">
                     <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
-                        <legend className="f1 fw6 ph0 mh0">Register</legend>
+                        <legend className="f1 fw6 ph0 mh0">Registrar</legend>
                         <div className="mt3">
-                            <label className="db fw6 lh-copy f6" htmlFor="name">Name</label>
+                            <label className="db fw6 lh-copy f6" htmlFor="name">Nome</label>
                             <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="text" name="name" id="name" />
                         </div>
                         <div className="mt3">
@@ -16,12 +16,12 @@ const Register = ({ onRouteChange }) => {
                             <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="email" name="email-address" id="email-address" />
                         </div>
                         <div className="mv3">
-                            <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
+                            <label className="db fw6 lh-copy f6" htmlFor="password">Senha</label>
                             <input className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="password" name="password" id="password" />
                         </div>
                     </fieldset>
                     <div className="">
-                        <input onClick={() => onRouteChange('home')} className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" type="submit" value="Register" />
+                        <input onClick={() => onRouteChange('home')} className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" type="submit" value="Registrar" />
                     </div>
                 </div>
             </main>
