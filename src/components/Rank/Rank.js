6399ec8d5d67c7579bb7d99e9933acb44bb34be7
Andre-Junior <andrejunior.dias@gmail.com>
2020-09-28 17:01:35 -0300

    Update entre
    updata entre os videos parte n gravada

--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -1,7 +1,15 @@
 import React from 'react'
 
-const Rank = () => {
+const Rank = ({name, entries}) => { 
     return (
+        <div>
+        {`${name} , sua classificação atual é...`}
+        <div className='white f1 '>
+        {entries}
+        </div>
+        </div>
+    )
+     /* (
         <div>
             <div className="white f3">
                 {'André, sua classificação atual é...'}
@@ -10,7 +18,7 @@ const Rank = () => {
                 {'#5'}
             </div>
         </div>
-    )
+    ) */
 }
 
 export default Rank
